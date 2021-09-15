202e3c3425f064c38c532f8d47c62571bf98f540
Jacinto Mba Cantero <jacmba@gmail.com>
2021-09-15 13:26:05 +0100

    Implemented plugin reload from menu

--- xPluginReloader/xPluginReloader.cpp
+++ xPluginReloader/xPluginReloader.cpp
@@ -1,11 +1,19 @@
 // Basic XPL skeleton
 
-#include <XPLMProcessing.h>
+#include <XPLMMenus.h>
+#include <XPLMPlugin.h>
 #include <string>
 
 #if IBM
 	#include <Windows.h>
 #endif // IBM
+
+int menuIdx;
+XPLMMenuID menu;
+
+void startMenu(void);
+void finishMenu(void);
+void menuHandler(void*, void*);
 
 /**********************************************
 	REQUIRED CALLBACKS
@@ -20,11 +28,15 @@ PLUGIN_API int XPluginStart(char *name, char *signature, char *description) {
 	strcpy_s(signature, sizeof(s), s);
 	strcpy_s(description, sizeof(d), d);
 
+	startMenu();
+
 	return 1;
 }
 
 // Called before the DLL is unloaded
-PLUGIN_API void XPluginStop(void) {}
+PLUGIN_API void XPluginStop(void) {
+	finishMenu();
+}
 
 // Called before the plugin is enabled - Returns 1 when successfully starts
 PLUGIN_API int XPluginEnable(void) {
@@ -36,3 +48,28 @@ PLUGIN_API void XPluginDisable(void) {}
 
 //Called by Plugin Manager when message is sent to us
 PLUGIN_API void XPluginReceiveMessage(XPLMPluginID from, int message, void *param) {}
+
+// Initialize menu
+void startMenu(void) {
+	XPLMMenuID pluginsMenu = XPLMFindPluginsMenu();
+	menuIdx = XPLMAppendMenuItem(pluginsMenu, "xPlugin Reloader", (void*)"PLUGINS_RELOAD_MENU", 0);
+	menu = XPLMCreateMenu("xPlugin Reloader", pluginsMenu, menuIdx, menuHandler, NULL);
+	XPLMAppendMenuItem(menu, "Reload all plugins", (void*)"RELOAD_PLUGINS", 0);
+}
+
+// Destroy menu
+void finishMenu(void) {
+	if (menu != NULL) {
+		XPLMDestroyMenu(menu);
+		menu = NULL;
+	}
+}
+
+// Handle menu actions
+void menuHandler(void* menuRef, void* itemRef) {
+	const char* action = (const char*)itemRef;
+
+	if(!strcmp(action, "RELOAD_PLUGINS")) {
+		XPLMReloadPlugins();
+	}
+}
